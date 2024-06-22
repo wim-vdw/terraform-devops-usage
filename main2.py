cd53c3410d17d5aaaa51517aa1be7c3e9236008f
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2024-06-22 12:06:20 +0200

    minor changes

--- main2.py
+++ main2.py
@@ -9,16 +9,22 @@ tfe_organization = os.environ.get('TFE_ORGANIZATION')
 tfe_token = os.environ.get('TFE_TOKEN')
 
 
-def build_search_text(name, provider):
-    return f'{tfe_domain_name}/{tfe_organization}/{name}/{provider}'
+def build_search_text(domain_name, organization, name, provider):
+    return f'{domain_name}/{organization}/{name}/{provider}'
 
 
 if __name__ == '__main__':
-    az_client = AzureDevOpsClient(organization=az_devops_organization, pat_token=az_devops_pat)
-    tf_client = TerraformClient(organization=tfe_organization, token=tfe_token, domain_name=tfe_domain_name)
+    az_client = AzureDevOpsClient(organization=az_devops_organization,
+                                  pat_token=az_devops_pat)
+    tf_client = TerraformClient(organization=tfe_organization,
+                                token=tfe_token,
+                                domain_name=tfe_domain_name)
     modules = tf_client.get_modules()
     for module in modules:
-        search_text = build_search_text(module['attributes']['name'], module['attributes']['provider'])
+        search_text = build_search_text(domain_name=tfe_domain_name,
+                                        organization=tfe_organization,
+                                        name=module['attributes']['name'],
+                                        provider=module['attributes']['provider'])
         print(search_text)
         results = az_client.search_code(az_devops_project, search_text)
         for result in results['results']:
