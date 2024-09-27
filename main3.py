4e532c96a219a809a3c1c0df84b49b78902b6586
Wim Van den Wyngaert <wim.vandenwyngaert@gmail.com>
2024-09-27 20:13:06 +0200

    add feature to check Azurerm version in all workspaces

--- main3.py
+++ main3.py
@@ -1,6 +1,6 @@
 import os
 import hcl2
-
+from collections import defaultdict
 from helpers import AzureDevOpsClient, TerraformClient
 
 az_devops_organization = os.environ.get('AZ_DEVOPS_ORGANIZATION')
@@ -10,11 +10,6 @@ tfe_domain_name = os.environ.get('TFE_DOMAIN_NAME')
 tfe_organization = os.environ.get('TFE_ORGANIZATION')
 tfe_token = os.environ.get('TFE_TOKEN')
 
-
-def build_search_text(domain_name, organization, name, provider):
-    return f'{domain_name}/{organization}/{name}/{provider}'
-
-
 if __name__ == '__main__':
     az_client = AzureDevOpsClient(organization=az_devops_organization,
                                   pat_token=az_devops_pat)
@@ -23,20 +18,21 @@ if __name__ == '__main__':
                                 domain_name=tfe_domain_name,
                                 verify=False)
     workspaces = tf_client.get_workspaces()
-    result = []
+    result = defaultdict(list)
     for workspace in workspaces:
-        if workspace['attributes']['name'] != 'nothing will be skipped':
-            tf_workspace = workspace['attributes']['name']
-            working_dir = workspace['attributes']['working-directory']
-            repo_url = workspace['attributes']['vcs-repo']['repository-http-url']
-            repo_id = str(workspace['attributes']['vcs-repo']['identifier']).split('/')[-1]
-            print(tf_workspace, '=>', repo_url, '=>', working_dir)
+        tf_workspace = str(workspace['attributes']['name'])
+        working_dir = workspace['attributes']['working-directory']
+        repo_url = workspace['attributes']['vcs-repo']['repository-http-url']
+        repo_id = str(workspace['attributes']['vcs-repo']['identifier']).split('/')[-1]
+        print(tf_workspace, '=>', repo_url, '=>', working_dir)
+        if tf_workspace.startswith('Squad-SAP-I'):
             files = az_client.get_files(az_devops_project, repo_id, scope_path=working_dir)
             if files:
                 for file in files['value']:
                     if 'isFolder' not in file and str(file['path']).lower().endswith('tf'):
                         content = az_client.get_file_content(az_devops_project, repo_id, file['path'])
                         print(file['path'])
+                        full_path = repo_url + '?path=' + file['path']
                         try:
                             content_parsed = hcl2.loads(content)
                             if 'terraform' in content_parsed:
@@ -48,10 +44,11 @@ if __name__ == '__main__':
                                             if 'azurerm' in provider:
                                                 version = provider['azurerm'].get('version', 'VERSION-NOT-SPECIFIED')
                                                 print(version)
-                                                result.append((tf_workspace, repo_id, file['path'], version))
+                                                result[tf_workspace].append((repo_id, file['path'], version, full_path))
                         except Exception as e:
                             print("Error parsing file, please check manually")
             else:
                 print('No files found!')
-    for item in result:
-        print(item)
+    for item in sorted(result):
+        for provider in result[item]:
+            print(item, provider)
